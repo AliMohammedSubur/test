62c711b92460ef67e64841b8b90f870efd110fed
AliMohammedSubur <110115505+AliMohammedSubur@users.noreply.github.com>
2022-09-20 14:41:14 +0000

    first commit

---
+++ test/Subtract.test.js
@@ -0,0 +1,7 @@
+const Subtract = require("./Subtract")
+
+
+test("Subtract two Numbers", () => {
+    expect("Subtract"(50, 24)
+    ).toBe(26)
+})
---
+++ test/Sum.test.js
@@ -0,0 +1,6 @@
+const Sum = require("./Sum")
+
+test("accurately add two numder", () => {
+    expect(Sum(67, 43)
+    ).toBe(110)
+})
